a26be1c25059d22842f33d06839bd1e8599f6de2
Grayson Gantek <grayson@inspry.com>
2025-08-22 22:35:53 -0400

    Add Wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- Font
+config.font_size = 16
+config.font = wezterm.font 'Gohu Font'
+
+-- Colors
+config.color_scheme = 'Predawn (Gogh)'
+config.window_background_opacity = 0.85
+
+-- Tabs
+config.use_fancy_tab_bar = false
+
+-- Disable Wayland
+config.enable_wayland = false
+
+return config
