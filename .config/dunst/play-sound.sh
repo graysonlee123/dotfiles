26a4c4d778dbfe2fd715e810f2971ea817c75d3a
Grayson Gantek <graysonleegantek@gmail.com>
2025-09-04 11:04:18 -0400

    Mute FileZilla notifications

--- .config/dunst/play-sound.sh
+++ .config/dunst/play-sound.sh
@@ -1,1 +1,13 @@
-/usr/bin/paplay /home/inspry/.config/dunst/caw.ogg
+#!/usr/bin/bash
+
+# Notification's app name, passed in by Dunst
+appname=$1
+
+function play () {
+  /usr/bin/paplay /home/inspry/.config/dunst/caw.ogg
+}
+
+if [[ "$appname" != "FileZilla" ]]; then
+  play
+fi
+
